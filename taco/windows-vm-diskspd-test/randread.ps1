0c96703a4d0d0a6757ccc04060e4dd6f24a987be
Heechul Kim <jijisa@iorchard.co.kr>
2020-10-09 18:20:55 +0900

    Add windows VM disk io test powershell scripts

---
+++ taco/windows-vm-diskspd-test/randread.ps1
@@ -0,0 +1,12 @@
+1..12 | ForEach-Object {
+    $param = "-o$_"
+    $result = c:\Users\IBK\diskspd\diskspd.exe -r -w0 -d600 -W300 -b4K -t8 $param -h -Z1M -c10G -L c:\randread.dat
+    foreach ($line in $result) { if ($line -like "total:*") { $total=$line; break } }
+    foreach ($line in $result) { if ($line -like "avg.*") { $avg=$line; break } }
+
+    $mbps = $total.Split("|")[2].Trim()
+    $iops = $total.Split("|")[3].Trim()
+    $latency = $total.Split("|")[4].Trim()
+    $cpu = $avg.Split("|")[1].Trim()
+    "Param $param, $iops iops, $mbps MB/s, $latency ms, $cpu CPU"
+}
